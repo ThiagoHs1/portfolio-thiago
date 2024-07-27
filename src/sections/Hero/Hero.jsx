15f7fdf50abce365779b9fb8dc62642c4bb2c663
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-07-27 16:18:16 -0300

    Lógica da troca de icon dark para light

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import styles from './HeroStyles.module.css'
 import thiago from '../../assets/thiago.jpeg'
-import themeIcon from '../../assets/sun.svg'
-import twitterIcon from '../../assets/twitter-light.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
+import sun from '../../assets/sun.svg'
+import moon from '../../assets/moon.svg'
+
+import twitterLight from '../../assets/twitter-light.svg'
+import githubLight from '../../assets/github-light.svg'
+import linkedinLight from '../../assets/linkedin-light.svg'
+
+import twitterDark from '../../assets/twitter-dark.svg'
+import githubDark from '../../assets/github-dark.svg'
+import linkedinDark from '../../assets/linkedin-dark.svg'
+
+
+
 import CV from '../../assets/CV - THIAGO HENRIQUE HEINLE SANCHEZ.pdf'
+import { useTheme } from '../../common/ThemeContext'
+
+
+// Hero component that displays the hero section with Thiago's info, image, icons, and download CV button.
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img src={thiago} alt="Thiago" className={styles.heroImage} />
-        <img src={themeIcon} alt="Modo escuro icon" className={styles.ColorMode} />
+        <img src={themeIcon} alt="Modo escuro icon" className={styles.ColorMode} onClick={toggleTheme} />
       </div>
       <div className={styles.info}>
         <h1 className={styles.title}>Thiago H Sanchez</h1>
