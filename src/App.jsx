11be16f8a320846f20658508d941f57b9c68c96c
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-07-28 15:40:43 -0300

    Finalizando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import './App.css'
-import Contact from './sections/Contact/Contact'
 import Footer from './sections/Footer/Footer'
 import Hero from './sections/Hero/Hero'
 import Projects from './sections/Projects/Projects'
