e9273df9d2554431df0be39c53e2cfda4728a66c
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-07-28 16:13:50 -0300

    Finalizando o projeto

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,37 +1,34 @@
 import React from 'react'
 import ProjectCard from '../../common/ProjectCard';
 import styles from './ProjectsStyles.module.css';
-import placeholder from '../../assets/placeholder.png';
+import efood from '../../assets/efood.png';
+import agenda from '../../assets/agenda.png';
+import sports from '../../assets/sports.png';
 
 
 function Projects() {
     return (
         <section id="projects" className={styles.container}>
-          <h1 className="sectionTitle">Projects</h1>
+          <h1 className="sectionTitle">Projetos</h1>
           <div className={styles.projectsContainer}>
             <ProjectCard
-              src={placeholder}
-              link="https://github.com/"
-              h3="Teste"
-              p="Teste"
+              src={efood}
+              link="https://github.com/ThiagoHs1/Efood/"
+              h3="Efood"
+              p="Aplicação web em react com api"
             />
             <ProjectCard
-              src={placeholder}
-              link="https://github.com/"
-              h3="Teste"
-              p="Teste"
+              src={sports}
+              link="https://github.com/ThiagoHs1/ebac_sports/"
+              h3="Sports website"
+              p="Aplicação em react para treinar uma loja virtual"
             />
+
             <ProjectCard
-              src={placeholder}
-              link="https://github.com/"
-              h3="Teste"
-              p="Teste"
-            />
-            <ProjectCard
-              src={placeholder}
-              link="https://github.com/"
-              h3="Teste 4"
-              p="Teste"
+              src={agenda}
+              link="https://github.com/ThiagoHs1/agenda-contatos/"
+              h3="Agenda de contatos"
+              p="Um dos primeiros projetos que fiz em javascript, uma aplicação simples de agenta de contato"
             />
           </div>
         </section>
